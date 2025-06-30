36020f222bd03ce61497bbe3e30080d3bbfbd5ff
partheevvv <parthivsaha8436@gmail.com>
2025-06-30 18:38:09 +0530

    Implemented validation and input sanitization

--- routes/summary.js
+++ routes/summary.js
@@ -1,7 +1,26 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
+const { query, validationResult } = require("express-validator");
 const { getSummary } = require("../controller/summaryController");
 
-router.get('/', auth, getSummary);
+const summaryValidator = [
+    query("month").optional().isInt({
+        min: 1,
+        max: 12
+    }).withMessage("Month should be between 1 and 12").toInt(),
+    query("year").optional().isInt({
+        min: 2000
+    }).withMessage("year must be a four-digit valid number").toInt()
+]
+
+const checkValidation = (req, res, next) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()) return res.json(400).json({
+        error: error.array()
+    });
+    next;
+}
+
+router.get('/', auth, summaryValidator, checkValidation, getSummary);
 
 module.exports = router;
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,13 +1,31 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
+const { body, query, validationResult } = require("express-validator");
 const { createTransaction, getTransaction, deleteTransaction, updateTransaction } = require("../controller/transactionController");
+
+
+const transactionValidators = [
+    body("date").exists().withMessage("Date is required").withMessage("Date should be in YYYY-MM-DD format").toDate(),
+    body("description").exists().withMessage("Description required").trim().escape(),
+    body("amount").exists().withMessage("Amount required").isFloat().toFloat(),
+    body("category").exists().withMessage("Category required").trim().escape(),
+    body("type").exists().withMessage("Type required").isIn(["Income", "Expense"]).withMessage("Type must be either income or expense").trim().escape(),
+];
+
+const checkValidation = (req, res, next) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()) return res.status(400).json({
+        error: error.array()
+    });
+    next();
+}
 
 router.use(auth);
 
-router.post("/", createTransaction);
+router.post("/", transactionValidators, checkValidation, createTransaction);
 router.get("/", getTransaction);
 router.delete("/:id", deleteTransaction);
-router.put("/:id", updateTransaction);
+router.put("/:id", transactionValidators, checkValidation, updateTransaction);
 
 module.exports = router;
 
